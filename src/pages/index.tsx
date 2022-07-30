a3cd6451ae48c45df31dc25a7d958ccf0a5218be
steniomoreira <steniomoreiradev@gmail.com>
2022-07-30 17:25:18 -0300

    refactor components

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <>
+            <Head> <title>Home | ig.news</title> </Head>
+            <h1>Simba is my dog</h1>    
+        </>
+    )
+}
