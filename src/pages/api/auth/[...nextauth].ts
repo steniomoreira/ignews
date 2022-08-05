9f7fe4cbf307331ae4d1e44d260c70021cb6fcb3
steniomoreira <steniomoreiradev@gmail.com>
2022-08-05 01:02:49 -0300

    feat: integrated faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { query as q } from "faunadb";
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -9,4 +12,20 @@ export default NextAuth({
       authorization: { params: { scope: 'read:user' } },
     }),
   ],
+  callbacks: {
+    async signIn({user, account, profile}) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'), { data: { email}}
+          )
+        )        
+        return true;        
+      } catch{
+       return false; 
+      }     
+    }
+  }
 })
